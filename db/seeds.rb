2f0c5f1f21e06c00e57e026aa2be6532f45ea9fa
anastasa27 <anastasiakonecky@gmail.com>
2014-12-11 13:36:13 -0500

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 Task.delete_all
 
-Task.create({ description: "Everlane Square zip wallet", completed: true })
-Task.create({ description: "Fragrance by Mandy Aftel" })
-Task.create({ description: "Knitty Gritty: Knitting for the Absolute Beginner" })
+Task.create({ description: "socks", completed: true })
+Task.create({ description: "World Peace" })
+
